96499584699dc354a9a8114c1a715a2a4107f4f9
Adli Fariz Bonaputra <abathz@gmail.com>
2018-04-30 08:51:40 +0700

    Update 30-04-18
    1. bab4 revisi => kelas diagram dan antarmuka
    2. bab 5 => on progress (sedang pengujian eksperimental)
    3. bab 6 => on progress
    4. perangkat lunak => mengganti throws menjadi try...catch
    5. membuat folder output untuk perangkat lunak

--- javadoc-to-latex/src/extractor/AttributeClassExtractor.java
+++ javadoc-to-latex/src/extractor/AttributeClassExtractor.java
@@ -4,31 +4,35 @@ import com.sun.javadoc.*;
 import java.io.*;
 
 /**
- * Kelas ini merupakan kelas untuk mengambil informasi sebuah atribut yang terdapat pada kelas
- * 
+ * Kelas ini merupakan kelas untuk mengambil informasi sebuah atribut yang
+ * terdapat pada kelas
+ *
  * @author Adli Fariz Bonaputra
  */
 public class AttributeClassExtractor {
 
   /**
-   * \textit{Method} ini akan menampilkan atribut-atribut yang dimiliki oleh sebuah kelas
+   * \textit{Method} ini akan menampilkan atribut-atribut yang dimiliki oleh
+   * sebuah kelas
+   *
    * @param fields sebuah array berisikan sejumlah atribut dari kelas
-   * @param out turunan dari kelas \texttt{Writer} yang digunakan untuk menulis file text
-   * @throws IOException 
+   * @param out turunan dari kelas \texttt{Writer} yang digunakan untuk menulis
+   * file text
    */
-  public static void extractAttributeClassContent(FieldDoc[] fields, BufferedWriter out) throws IOException {
-    out.write("Atribut yang dimiliki kelas ini adalah sebagai berikut.\n");
-    if (fields.length == 0) {
-      out.write("\\begin{itemize}\n");
-      out.write("\\item Tidak memiliki Atribut" + "\n");
-      out.write("\\end{itemize}\n");
-    } else {
-      out.write("\\begin{itemize}\n");
-      for (int j = 0; j < fields.length; j++) {
-        Type type = fields[j].type();
-        out.write("\\item \\texttt{" + type.typeName() + " " + fields[j].name() + "} - " + fields[j].commentText() + "\n");
+  public static void extractAttributeClassContent(FieldDoc[] fields, BufferedWriter out) {
+    try {
+      if (fields.length == 0) {
+        out.write("Kelas ini tidak memiliki atribut. ");
+      } else {
+        out.write("Atribut yang dimiliki kelas ini adalah sebagai berikut.\n");
+        out.write("\\begin{itemize}\n");
+        for (int j = 0; j < fields.length; j++) {
+          Type type = fields[j].type();
+          out.write("\\item \\texttt{" + type.typeName() + " " + fields[j].name() + "} - " + fields[j].commentText() + "\n");
+        }
+        out.write("\\end{itemize}\n");
       }
-      out.write("\\end{itemize}\n");
+    } catch (IOException e) {
     }
   }
 }
--- javadoc-to-latex/src/extractor/ClassExtractor.java
+++ javadoc-to-latex/src/extractor/ClassExtractor.java
@@ -15,20 +15,21 @@ public class ClassExtractor {
    * \textit{Method} ini akan menampilkan nama kelas berserta penjelasan dari sebuah kelas
    * @param classes sebuah array berisikan sejumlah kelas
    * @param out turunan dari kelas \texttt{Writer} yang digunakan untuk menulis file text
-   * @throws IOException 
    */
-  public static void extractClassContent(ClassDoc[] classes, BufferedWriter out) throws IOException {
-    for (ClassDoc classDoc : classes) {
-//      System.out.println(classDoc.superclass().name());
+  public static void extractClassContent(ClassDoc[] classes, BufferedWriter out){
+    try {
+      for (ClassDoc classDoc : classes) {
 
       out.write("\\item \\texttt{" + classDoc.name() + "}\\\\ \n");
-      out.write(classDoc.commentText() + "\n\n");
+      out.write(classDoc.commentText() + ".\n\n");
 
       FieldDoc[] fields = classDoc.fields(false);
       AttributeClassExtractor.extractAttributeClassContent(fields, out);
 
       MethodDoc[] methods = classDoc.methods(false);
-      MethodExtractor.extractMethodContent(classDoc, methods, out);
+      MethodClassExtractor.extractMethodClassContent(classDoc, methods, out);
+    }
+    } catch (IOException e){
     }
   }
 }
